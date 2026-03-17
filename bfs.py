b124983b9d319eb46d606814bdd377b14e46d5a8
Vinayaka J <vinijkrn96645@gmail.com>
2026-03-17 10:06:50 +0530

    Initial commit

---
+++ bfs.py
@@ -0,0 +1,31 @@
+from collections import deque
+
+def bfs(graph, start):
+    visited = []
+    queue = deque()
+
+    visited.append(start)
+    queue.append(start)
+
+    while queue:
+        node = queue.popleft()
+        print(node, end=" ")
+
+        for neighbour in graph[node]:
+            if neighbour not in visited:
+                visited.append(neighbour)
+                queue.append(neighbour)
+
+# Graph with different alphabet nodes
+graph = {
+    'P': ['Q', 'R'],
+    'Q': ['S', 'T'],
+    'R': ['U', 'V'],
+    'S': [],
+    'T': [],
+    'U': [],
+    'V': []
+}
+
+# Start BFS
+bfs(graph, 'P')
